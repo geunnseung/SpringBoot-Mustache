eb1a68f7ae0f6f3589cec5c938c49aaf8873a134
seunggeun <bsg1017@naver.com>
2022-11-08 13:21:00 +0900

    SprintBoot MVC -mustache

---
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,28 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "geun");
+        //view값 넘기기
+        return "greetings";
+        // greetings라는 이름의 view를 리턴
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon2(@PathVariable String id, Model model) {
+        model.addAttribute("username", "geun");
+        // view에 값을 넘기기
+        model.addAttribute("id", id);
+        // view에 값을 넘기기
+        return "greetings";
+        // greetings라는 이름의 view를 리턴
+    }
+
+}
